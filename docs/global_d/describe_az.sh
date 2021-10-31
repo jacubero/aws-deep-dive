11353e723544ca0ccca816933fa62c05689db3fd
jaacubero@gmail.com <jaacubero@gmail.com>
2021-10-31 14:42:21 +0100

    New version

--- docs/global_d/describe_az.sh
+++ docs/global_d/describe_az.sh
@@ -1,6 +1,7 @@
-echo "RegionName,State,ZoneId,ZoneName"
+echo "Region,RegionName,Visibility:q
+"
 
-tail -n +2 regions.csv | while IFS=, read -r RegionName State ZoneId ZoneName
+tail -n +2 regions.csv | while IFS=, read -r Region RegionName Visibility
 do
-	aws ec2 describe-availability-zones --region $RegionName | cut -f 2-5 -d$'\t' --output-delimiter=','
+	aws ec2 describe-availability-zones --region $RegionName --output text | cut -f 2,8 -d$'\t' --output-delimiter=','
 done
